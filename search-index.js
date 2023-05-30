0aadaa3e8debee154f664cef95b0e0b624768b1d
Techie-Pi <Techie-Pi@users.noreply.github.com>
2023-05-30 16:25:40 +0000

    deploy: 311cb3121528347dfb3b03a5f7a6470ee5ca08ae

---
+++ search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = JSON.parse('{\
+"doxygen_rs":{"doc":"Simple Doxygen to Rustdoc translation.","t":"AFF","n":["generator","transform","rustdoc"],"q":["doxygen_rs","","doxygen_rs::generator"],"d":["","This function transforms the Doxygen of a single element …","Creates a Rustdoc string from a Doxygen string."],"i":[0,0,0],"f":[0,[1,2],[2,[[3,[2,0]]]]],"p":[[15,"str"],[3,"String"],[4,"Result"]]}\
+}');
+if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
+if (typeof exports !== 'undefined') {exports.searchIndex = searchIndex};
---
+++ source-files.js
@@ -0,0 +1,4 @@
+var sourcesIndex = JSON.parse('{\
+"doxygen_rs":["",[],["emojis.rs","generator.rs","lexer.rs","lib.rs","parser.rs"]]\
+}');
+createSourceSidebar();
